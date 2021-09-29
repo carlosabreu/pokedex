7218867df2c6db645b2063adcb651fd375e15421
F1729968 Carlos Abreu Custodio <carlos.abreu@bb.com.br>
2021-09-29 11:52:55 -0300

    Protegendo o app para nao quebrar quando nao encontra descricao ou a imagem

--- app/src/main/java/com/quintatecnica/pokedex/model/Pokemon.kt
+++ app/src/main/java/com/quintatecnica/pokedex/model/Pokemon.kt
@@ -1,6 +1,7 @@
 package com.quintatecnica.pokedex.model
 
 import android.content.Context
+import com.quintatecnica.pokedex.R
 
 class Pokemon(
     val number: Int,
@@ -10,11 +11,15 @@ class Pokemon(
     override fun toString() = "$number - $name, $type"
 
     fun image(context: Context): Int {
-        return context.resources.getIdentifier(name.lowercase(), "mipmap", context.packageName)
+        val identifier =
+            context.resources.getIdentifier(name.lowercase(), "mipmap", context.packageName)
+        return if (identifier == 0) R.mipmap.unkown_pokemon else identifier
     }
 
     fun description(context: Context): Int {
         val descriptionString = "pokemon_description_${name.lowercase()}"
-        return context.resources.getIdentifier(descriptionString, "string", context.packageName)
+        val identifier =
+            context.resources.getIdentifier(descriptionString, "string", context.packageName)
+        return if (identifier == 0) R.string.pokemon_description_missing else identifier
     }
 }
